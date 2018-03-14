509d42373ece8ba824f8fe49bac68792e6e1defc
basbeu <bastien.beuchat@epfl.c>
2018-03-14 10:12:09 +0100

    adding hashtable.c

---
+++ done/hashtable.c
@@ -0,0 +1,20 @@
+/**
+ * @file hashtable.c
+ * @brief 
+ *
+ * @author Bastien Beuchat and Andrea Scalisi
+ */
+
+#include "hashtable.h"
+
+error_code add_Htable_value(Htable_t table, pps_key_t key, pps_value_t value){
+
+}
+
+pps_value_t get_Htable_value(Htable_t table, pps_key_t key){
+	
+}
+
+size_t hash_function(pps_key_t key, size_t table_size){
+
+}
