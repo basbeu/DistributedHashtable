52d81a127bf06647e22c80afd03baecba405d16c
basbeu <bastien.beuchat@epfl.c>
2018-03-14 10:39:22 +0100

    simple implementation of hashtable.c

---
+++ done/error.h
@@ -0,0 +1,1 @@
+../provided/week04/error.h
--- done/hashtable.c
+++ done/hashtable.c
@@ -5,16 +5,23 @@
  * @author Bastien Beuchat and Andrea Scalisi
  */
 
+#include "error.h"
 #include "hashtable.h"
 
 error_code add_Htable_value(Htable_t table, pps_key_t key, pps_value_t value){
-
+	if(table == NULL){
+		return ERR_BAD_PARAMETER;
+	}
+	
+	table[hash_function(key, HTABLE_SIZE)] = value;
+	 
+	return ERR_NONE;
 }
 
 pps_value_t get_Htable_value(Htable_t table, pps_key_t key){
-	
+	return table[hash_function(key, HTABLE_SIZE)];
 }
 
 size_t hash_function(pps_key_t key, size_t table_size){
-
+	return key % table_size;
 }
