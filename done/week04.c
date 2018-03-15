6296b6cdcfdf242e959fbe601df43ed02e1e4b69
basbeu <bastien.beuchat@epfl.c>
2018-03-15 13:37:32 +0100

    adding customs tests

--- done/week04.c
+++ done/week04.c
@@ -24,8 +24,27 @@ START_TEST(add_value_does_retrieve_same_value)
 	const pps_value_t valeur_lue = get_Htable_value(table, cle);
 
 	ck_assert_int_eq(valeur_mise, valeur_lue);
+	
+	
 }
 END_TEST
+
+START_TEST(hash_value_bigger_than_HTABLE_SIZE)
+{
+	const pps_key_t cle = 'c';
+	ck_assert_int_eq(0,hash_function(cle, 8000));
+}
+END_TEST
+
+START_TEST(add_Htable_value_null)
+{
+	const pps_key_t cle = 'c';
+	const pps_value_t valeur_mise = 42;
+	error_code code =  add_Htable_value(NULL, cle, valeur_mise);
+	ck_assert_bad_param(code);
+}
+END_TEST
+
 
 Suite *hashtable_suite()
 {
@@ -36,7 +55,9 @@ Suite *hashtable_suite()
     suite_add_tcase(s, tc_ht);
 
     tcase_add_test(tc_ht, add_value_does_retrieve_same_value);
-
+	tcase_add_test(tc_ht, hash_value_bigger_than_HTABLE_SIZE);
+	tcase_add_test(tc_ht,add_Htable_value_null);
+	
     return s;
 }
 
