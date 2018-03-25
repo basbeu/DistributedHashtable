955f3c058ba5012fdab5803c9fb0674a5af59a79
basbeu <bastien.beuchat@epfl.c>
2018-03-25 16:10:34 +0200

    fix and clean network

--- done/network.c
+++ done/network.c
@@ -7,35 +7,39 @@
  */
 #include "network.h"
 
-error_code network_get(client_t client, pps_key_t key, pps_value_t *value){
-	unsigned int out_msg;
-    //out_msg = htonl(key);
-	sendto(client.socket, &key, 1, 0,(struct sockaddr *)&client.node.srv_addr, sizeof(client.node.srv_addr));
+error_code send_request(client_t client,pps_key_t* data, pps_value_t *value, size_t size_data){
+	M_REQUIRE_NON_NULL(data);
+	M_REQUIRE_NON_NULL(value);
 	
+	error_code error = ERR_NONE;
+	
+	ssize_t out_msg_len = sendto(client.socket, data, size_data, 0,(struct sockaddr *)&client.node.srv_addr, sizeof(client.node.srv_addr));
+		
     ssize_t in_msg_len = recv(client.socket, value, sizeof(value), 0);
-    //*value = ntohl(*value);
+	
+	if (out_msg_len == -1 || in_msg_len == -1){
+		error = ERR_NETWORK;
+	}
+	
+	return error;
+}
+
+error_code network_get(client_t client, pps_key_t key, pps_value_t *value){
+	M_REQUIRE_NON_NULL(value);
+
+    return send_request(client, &key,value, 1);
 }
 
 error_code network_put(client_t client, pps_key_t key, pps_value_t value){
-	//unsigned long msg = key;
-	//msg <<= 32;
-	//msg = msg|value;
 	unsigned char out_msg[5];
 	out_msg[0] = key;
-	debug_print("network key : %d value : %d", key,value);
-
+	
     value = htonl(value);
     
-	debug_print("network key : %d value : %d", key,value);
-    for(size_t i = 0; i < 4; ++i){
+	for(size_t i = 0; i < 4; ++i){
 		out_msg[i+1] = (char) value;
 		value >>= 8;
 	} 
-    
-    
-    
-	sendto(client.socket, &out_msg, 5, 0,(struct sockaddr *)&client.node.srv_addr, sizeof(client.node.srv_addr));
-	
-	ssize_t in_msg_len = recv(client.socket, value, sizeof(value), 0);
-}
 
+	return send_request(client, out_msg, &value, 5);   
+}
